ae4d301060111546eea417d35d39cd1ec99b53e7
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-10 08:55:44 -0700

    this program prints all possible combinations of two-digit numbers in ascending order. xy and yx are considered the same combination. Print only the smaller of the two combinations.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+	int j;
+
+	while (i <= '8')
+	{
+		for (j = '1'; j <= '9'; j++)
+		{
+			if (i == j)
+				j++;
+
+			putchar(i);
+			putchar(j);
+
+			if (i != '8' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		i++;
+	}
+
+	putchar('\n');
+
+	return 0;
+}
+
