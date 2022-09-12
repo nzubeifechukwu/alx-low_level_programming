54d354396edbc19872fb72191db1ac8d364a4be8
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-12 02:45:48 -0700

    prints a string to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,11 +10,13 @@
  * man (3) printf or man (3) puts. Your program should return 1. Your program
  * should compile without any warnings when using the -Wall gcc option
  *
- * Return: 1
+ * Return: 1 (Error)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char err_mess[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(err_mess, sizeof(err_mess), 1, stderr);
 
 	return (1);
 }
