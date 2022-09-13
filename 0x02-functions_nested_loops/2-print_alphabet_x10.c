3c3a97610ea83f0bb0259eb771459ee3d0126f4f
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-13 05:42:47 -0700

    main.h contains the prototypes for the user-defined functions; 2-print_alphabet_x10.c prints lowercase alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		print_alphabet();
+}
+
