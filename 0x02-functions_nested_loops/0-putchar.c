9d1c1f0318d5ec78c61b3b7e3ddd72ab4994d870
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-13 02:57:19 -0700

    this program uses a user-defined file to print a string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "_putchar.h"
+
+/**
+ * main - prints _putchar, followed by a new line
+ *
+ * This program uses a user-defined function to print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+
+	_putchar('\n');
+
+	return 0;
+}
+
