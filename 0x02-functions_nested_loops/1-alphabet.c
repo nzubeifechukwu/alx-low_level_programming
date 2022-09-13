f6c0f4e7ff5036bf2e1db8ac471835fbdda8b09c
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-13 04:03:30 -0700

    main.h contains the prototypes for the user-defined functions; 1-alphabet.c prints lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet: prints the lowercase alphabets
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char low;
+	for (low = 'a'; low <= 'z'; low++)
+		_putchar(low);
+
+	_putchar('\n');
+}
