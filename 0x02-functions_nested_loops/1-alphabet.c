fc19749bb37415613520bd9de3550cbfc70a06be
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-13 04:12:00 -0700

    made a slight change

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 #include "main.h"
 
 /**
- * print_alphabet: prints the lowercase alphabets
+ * print_alphabet - prints the lowercase alphabets
  *
  * Return: void
  */
 void print_alphabet(void)
 {
 	char low;
+
 	for (low = 'a'; low <= 'z'; low++)
 		_putchar(low);
 
