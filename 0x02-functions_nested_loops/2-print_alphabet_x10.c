a8a9dbd42b896fccef42197bd398056f823e665e
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-13 05:53:14 -0700

    _putchar.c is a user-defined function that prints single characters; main.h contains the prototypes for the user-defined functions; 2-print_alphabet_x10.c prints lowercase alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char low;
 
 	for (i = 0; i <= 9; i++)
-		print_alphabet();
+	{
+		for (low = 'a'; low <= 'z'; low++)
+			_putchar(low);
+		
+		_putchar('\n');
+	}
 }
 
