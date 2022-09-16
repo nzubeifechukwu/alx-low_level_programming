c011fadb9ed6d3700a3610173a720142b0c16259
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-15 20:27:40 -0700

    .

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,22 +1,32 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line using the backslash \
- * @n: number of times \ will be printed
+ * print_diagonal - draws a diagonal line using the backslash
+ * @n: number of times backslash will be printed
  *
  * Return: void
  */
 void print_diagonal(int n)
 {
-	int i, j;
+	int i = 0, j;
 
-	for (i = 0; i < n; i++)
+	if (n < 1)
 	{
-		_putchar('\\');
 		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			j = i;
 
-		for (j = 0; j < i; j++)
-			_putchar(' ');
+			while (j--)
+				_putchar(' ');
+
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
 	}
 }
 
