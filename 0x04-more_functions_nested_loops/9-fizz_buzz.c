3d2411db20c616b70c64e49fa0fc06343cd2d682
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-15 18:22:39 -0700

    .

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100, replacing multiples of 3, 5, and 3 and 5 with Fizz,
+ * Buzz and FizzBuzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
