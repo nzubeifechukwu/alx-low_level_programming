75ee465fa4f58f3753f18bcaedac5f6c4bcff661
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-16 09:32:41 -0700

    .

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - prints the first n Fibonacci numbers, starting with 1 and 2
+ * @n: size of the Fibonnaci sequence. It should not exceed 50
+ *
+ * Return: void
+ */
+void fibonacci(int n)
+{
+	int i, fib[51] = {1, 2};
+
+	for (i = 2; i < n; i++)
+		fib[i] = fib[i - 1] + fib[i - 2];
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", fib[i]);
+		else
+			printf(", %d", fib[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * main - prints the first 50 Fibonacci numbers starting with 1 and 2
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	fibonacci(50);
+
+	return (0);
+}
+
