5600277113d482c2f7e7940f82f476a294defc62
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-19 03:40:12 -0700

    .

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: string s
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	int i = 0;
+
+	/* if (s[0] == *s) */
+	/*	len++; */
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	return (len);
+}
+
