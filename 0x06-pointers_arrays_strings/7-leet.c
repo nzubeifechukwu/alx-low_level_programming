98838166d42f9acfa34f2e8e1258956208789f55
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-22 09:47:33 -0700

    updating main.h and adding 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+/**
+ * leet - encodes a string into leet or 1337
+ * @str: string to encode
+ *
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ * You can only use one if in your code
+ * You can only use two loops in your code
+ * You are not allowed to use switch
+ * You are not allowed to use any ternary operation
+ *
+ * Return: encode string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char low[] = "aeotl";
+	char up[] = "AEOTL";
+	char dig[] = "43071";
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		for (j = 0; *(up + j) != '\0'; j++)
+		{
+			if (*(str + i) == *(low + j) || *(str + i) == *(up + j))
+				*(str + i) = *(dig + j);
+		}
+	}
+
+	return (str);
+}
+
