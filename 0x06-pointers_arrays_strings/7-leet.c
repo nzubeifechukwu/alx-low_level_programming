eac0897c96fa9365aa8440007ce1077b60721143
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-23 11:28:13 -0700

    updating 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,16 +2,6 @@
  * leet - encodes a string into leet or 1337
  * @str: string to encode
  *
- * Letters a and A should be replaced by 4
- * Letters e and E should be replaced by 3
- * Letters o and O should be replaced by 0
- * Letters t and T should be replaced by 7
- * Letters l and L should be replaced by 1
- * You can only use one if in your code
- * You can only use two loops in your code
- * You are not allowed to use switch
- * You are not allowed to use any ternary operation
- *
  * Return: encoded string
  */
 char *leet(char *str)
