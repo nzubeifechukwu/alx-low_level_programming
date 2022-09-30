37608b6eee80e852da14bd4e20990bba9ceac1b0
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-09-30 14:03:46 -0700

    adding 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define UNUSED(argv) (void)(argv)
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	UNUSED(argv);
+
+	printf("%d\n", argc);
+	return (0);
+}
