c128fceba4604882d8dc93bb275d975e40c38e12
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-05 19:50:08 -0700

    updating main.h and adding 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2-d array of integers
+ * @width: grid width or columns
+ * @height: grid height or rows
+ *
+ * Return: a height * width 2-d array
+ */
+int **alloc_grid(int width, int height)
+{
+	int w, h, **grid;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	/* allocating memory space for grid rows */
+	grid = malloc(sizeof(int) * height);
+
+	/* allocating memory space for grid columns */
+	for (h = 0; h < height; h++)
+		grid[h] = malloc(sizeof(int) * width);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (h = 0; h < height; h++)
+	{
+		for (w = 0; w < width; w++)
+			grid[h][w] = 0;
+	}
+
+	return (grid);
+}
