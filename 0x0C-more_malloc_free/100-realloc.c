38e7bae934b61884b8dbab1c626e02b5b9cc41ac
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-08 21:20:55 -0700

    updating 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,13 +22,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
+	new_ptr = malloc(new_size);
+
 	if (ptr == NULL)
 	{
-		new_ptr = malloc(new_size);
-
-		if (new_ptr == NULL)
-			return (NULL);
+		return (new_ptr);
 	}
+
+	if (new_ptr == NULL)
+		return (new_ptr);
 
 	if (new_size > old_size)
 	{
@@ -44,6 +46,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			((char *)new_ptr)[i] = ((char *)ptr)[i];
 		}
 	}
+
 	free(ptr);
 	return (new_ptr);
 }
