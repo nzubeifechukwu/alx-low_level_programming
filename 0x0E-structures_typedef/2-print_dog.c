41366aa76956392a3e5e422b3c5413a0680fa10f
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-10 10:26:42 -0700

    updating 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,21 +10,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		if (d->name)
-			printf("Name: %s\n", d->name);
-		else
-			puts("Name: (nil)");
+	if (d == NULL)
+		return;
 
-		if (d->age)
-			printf("Age: %.6f\n", d->age);
-		else
-			puts("Age: (nil)");
-
-		if (d->owner)
-			printf("Owner: %s\n", d->owner);
-		else
-			puts("Owner: (nil)");
-	}
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->name : "(nil)");
 }
