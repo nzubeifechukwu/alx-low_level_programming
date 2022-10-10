0c0eada482447357794baaeefd89c33fec7e9cca
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-10 10:04:30 -0700

    updating dog.h and adding 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints the attributes of a struct dog
+ * @d: pointer to the struct dog
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	/* d = malloc(sizeof(*d)); */
+
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			puts("Name: (nil)");
+
+		if (d->age)
+			printf("Age: %.6f\n", d->age);
+		else
+			puts("Age: (nil)");
+
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			puts("Owner: (nil)");
+	}
+}
