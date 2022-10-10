9cdeca943924f72f31c8a563f590af492ef91f44
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-10 08:38:25 -0700

    adding README.md and dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: dog is an animal
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */
