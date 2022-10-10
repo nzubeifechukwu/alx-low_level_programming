60cf199d21a11b7629caae87f91441d3e547a7c3
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-10 13:55:55 -0700

    updating 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,30 @@
 #include <stdlib.h>
 
 /**
- * new_dog - creates a new dog
+ * _strcpy - copies a string
+ * @s: string to copy
+ *
+ * Return: pointer to new copy of string
+ */
+char *_strcpy(char *s)
+{
+	char *scp;
+	int i;
+
+	scp = malloc(sizeof(*s) + 1);
+
+	if (scp == NULL)
+		return (NULL);
+
+	for (i = 0; *(s + i); i++)
+		scp[i] = s[i];
+
+	scp[i] = '\0';
+	return (scp);
+}
+
+/**
+ * new_dog - creates a new dog_t
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
@@ -11,14 +34,27 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ndog = malloc(sizeof(dog_t));
+	dog_t *ndog = malloc(sizeof(*ndog));
 
 	if (ndog == NULL)
 		return (NULL);
 
-	ndog->name = name;
+	ndog->name = _strcpy(name);
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+
 	ndog->age = age;
-	ndog->owner = owner;
+
+	ndog->owner = _strcpy(owner);
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
 
 	return (ndog);
 }
