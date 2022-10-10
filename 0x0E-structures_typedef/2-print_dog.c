1c5560c21a396ee1b2b55d25777b886c6f6b9ed8
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-10 10:31:40 -0700

    updating 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,5 @@ void print_dog(struct dog *d)
 
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner ? d->name : "(nil)");
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
