1ecb6dd4f4a90c2a5f706988b35c54597f49d8f2
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-12 05:31:51 -0700

    updating 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
