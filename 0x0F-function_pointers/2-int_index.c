8c1abf21739b97d8302b3dbaf65d3da6c17a1a9c
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-12 05:43:19 -0700

    updating 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,7 +2,7 @@
  * int_index - searches for an integer
  * @array: integer array to search for an integer
  * @size: number of elements in array
- * @cmp(int): function to use to compare values
+ * @cmp: function to use to compare values
  *
  * Return: index of 1st matching elem, -1 if size <= 0 or no matching elem
  */
@@ -10,10 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
+	if (cmp && array)
 	{
-		if (cmp(array[i]))
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
 	return (-1);
 }
