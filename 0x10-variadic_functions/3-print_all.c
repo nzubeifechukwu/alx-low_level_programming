4a4107ffd55b09295eb6c53c2294f6a60b6aedc5
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-14 06:35:52 -0700

    updating variadic_functions.h and adding 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of argument types passed to function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int c = 0;
+	char *s, *sep = "";
+
+	va_start(ap, format);
+
+	while (*(format + c))
+	{
+		switch (*(format + c))
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(ap, int)); /* chars are promoted to int */
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(ap, double)); /* floats promotd to doub */
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", sep, s);
+				break;
+		}
+
+		sep = ", ";
+		c++;
+	}
+
+	printf("\n");
+	va_end(ap);
+}
