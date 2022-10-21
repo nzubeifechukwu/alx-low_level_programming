535be42c9dd4e36e2425707eca296c63bd650956
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-20 22:50:37 -0700

    Update lists.h and add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * premain - executes before the main function
+ */
+void premain(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
