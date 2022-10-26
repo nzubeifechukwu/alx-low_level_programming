1cda8cd35db0deeb83ba989abdd1bf056655696f
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-26 01:58:30 -0700

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (i < idx - 1)
 	{
-		if (holder->next == NULL)
+		if (holder == NULL)
 			return (NULL);
 		holder = holder->next;
 		i++;
