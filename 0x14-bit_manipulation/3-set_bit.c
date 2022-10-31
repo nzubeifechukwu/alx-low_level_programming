234c4735def64e4d38cda56cb3fe8568fba869fa
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-10-31 07:53:23 -0700

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (!(*n & (1 << index)))
-	{
-		*n = *n | (1 << index);
-		return (1);
-	}
-	return (-1);
+	if (index > 63)
+		return (-1);
+
+	*n = *n | (1 << index);
+
+	return (1);
 }
