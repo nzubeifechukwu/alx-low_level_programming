686099b9a04032c269397f0144d3684d6837697d
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-01-07 23:45:56 -0800

    Add 3-hash_table_set.c and hash_tables.h

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * create_array_item - creates a table array item
+ * @key: item key
+ * @value: value associated with key
+ *
+ * Return: created item on success, NULL on fail
+ */
+hash_node_t *create_array_item(const char *key, const char *value)
+{
+	hash_node_t *array_item;
+
+	if (strlen(key) == 0)
+		return (NULL);
+
+	array_item = malloc(sizeof(hash_node_t));
+
+	if (array_item == NULL)
+		return (NULL);
+
+	array_item->key = malloc(strlen(key) + 1);
+	array_item->value = malloc(strlen(value) + 1);
+
+	strcpy(array_item->key, key);
+	strcpy(array_item->value, value);
+	array_item->next = NULL;
+
+	return (array_item);
+}
+
+/**
+ * hash_table_set - adds an element to a hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *array_item, *curr_arr_item;
+
+	array_item = create_array_item(key, value);
+
+	if (array_item == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	curr_arr_item = ht->array[index];
+
+	if (curr_arr_item == NULL)
+	{
+		ht->array[index] = array_item;
+	}
+	else
+	{
+		if (strcmp(curr_arr_item->key, key) == 0)
+			strcpy(ht->array[index]->value, value);
+		else
+			return (0);
+	}
+	return (1);
+}
