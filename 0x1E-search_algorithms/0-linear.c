7ea594f1b2f5c7c1bbc097ac340946f7fb31aa35
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-04-13 07:22:19 -0700

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,14 +13,11 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (*array)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			printf("Value checked array[%ld] = [%d]\n", i, *(array + i));
-			if (*(array + i) == value)
-				return (i);
-		}
+		printf("Value checked array[%ld] = [%d]\n", i, *(array + i));
+		if (*(array + i) == value)
+			return (i);
 	}
 	return (-1);
 }
